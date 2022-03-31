339c105f091dddef6e298eb321cd5a575d87ebb3
Ramazan <raaaaaamazan@gmail.com>
2022-03-31 19:04:15 +0300

    Modul paratigi

---
+++ module/circle.js
@@ -0,0 +1,9 @@
+const circleArea = (radius) => {
+    console.log (Math.PI * radius ** 2);
+}
+
+const circleCircumference = (radius) => {
+    console.log(2 * Math.PI * radius) ;
+}
+
+module.exports = {circleArea,circleCircumference}
