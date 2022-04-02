0df1ccb553ddbc4d613b4f57f4569024c1c652f4
Ramazan <raaaaaamazan@gmail.com>
2022-04-02 15:10:14 +0300

    my web server

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const http = require("http")
+
+const server = http.createServer((req,res)=>{
+   const url = req.url;
+
+   if(url == "/"){
+       res.writeHead(200 , {"Content-Type" : "text/html"});
+       res.write("<h2>Deneme Sayfasina Hosgeldiniz</h2>")
+
+   }
+   else if (url == "/hakkimda"){
+    res.writeHead(200 , {"Content-Type" : "text/html"});
+    res.write("<h2>Hakkimda Sayfasi</h2>")
+
+   }
+   else if (url == "/iletisim"){
+    res.writeHead(200 , {"Content-Type" : "text/html"});
+    res.write("<h2>iletisim Sayfasi</h2>")
+
+   }
+   else{
+    res.writeHead(404 , {"Content-Type" : "text/html"});
+    res.write("<h1>404</h1>")
+   
+   }
+   res.end();
+})
+
+
+const port = 5000
+server.listen(port ,()=>{
+    console.log(` ${port} portundan dinleniliyor `)
+})
